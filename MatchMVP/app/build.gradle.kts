046aee4b13951623b5df74db8417e6dca1c5b217
Roman7725 <bykovroman09@gmail.com>
2026-08-02 17:00:09 +0200

    Update build.gradle.kts

--- MatchMVP/app/build.gradle.kts
+++ MatchMVP/app/build.gradle.kts
@@ -29,12 +29,16 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 }
 
@@ -43,6 +47,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // Coroutines (ОБЯЗАТЕЛЬНО для MainActivity & await())
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.0")
 
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:33.1.2"))
