663e42c318ae6e829ad614b022063f7dededc439
Roman7725 <bykovroman09@gmail.com>
2026-08-04 20:45:48 +0200

    Create NearbyPeer.kt

---
+++ MatchMVP/app/src/main/java/com/matchmvp/app/NearbyPeer.kt
@@ -0,0 +1,18 @@
+package com.matchmvp.app
+
+data class NearbyPeer(
+    val uid: String,
+    val nickname: String,
+    val status: String,
+    val likedTargetUid: String,
+    val contactInfo: String,
+    val rssi: Int,
+    val lastSeen: Long = System.currentTimeMillis()
+)
+
+data class UiPeer(
+    val uid: String,
+    val avatarLabel: String,
+    val liked: Boolean,
+    val hasBadge: Boolean
+)
